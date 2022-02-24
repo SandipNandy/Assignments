f777084810b6a2084c69725cca90702119f0cb97
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-24 14:31:21 +0530

    2nd commit
    directory has been changed

--- Assignment8.java
+++ Assignment8.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 public class Assignment8 {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		//Autocomplete drop down assignment
